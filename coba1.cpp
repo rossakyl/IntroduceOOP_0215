b72c61faff6fb0646b816a75ddfb715e90613671
rossakyl <rossakayla01@gmail.com>
2025-04-28 18:15:25 +0700

    Access modifier

--- coba1.cpp
+++ coba1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class planet {
+    private:
+            float gravitasi;
+    public:
+    string nam;
+    float diameter;
+
+
+
+};
 int main()
 {
     
