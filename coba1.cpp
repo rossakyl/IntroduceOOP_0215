65d2bc0400e9a062d3b613fb0744a0d2ed4164a2
rossakyl <rossakayla01@gmail.com>
2025-04-28 19:13:37 +0700

    add class gravitasi

--- coba1.cpp
+++ coba1.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 using namespace std;
 
-class planet {
-    private:
-            float gravitasi;
-    public:
-    string nam;
+class planet
+{
+private:
+    float gravitasi;
+
+public:
+    string nama;
     float diameter;
 
-
-
+    void displaydata()
+    {
+        cout << "gravitasiku " << gravitasi << endl;
+    }
+    void inputdata()
+    {
+        cout << "input nama : ";
+        cin >> nama;
+        cout << "ukuranku : ";
+        cin >> diameter;
+        cout << "gravitasiku : ";
+        cin >> gravitasi;
+    }
 };
 int main()
 {
-    
+    planet venus;
+    venus.inputdata();
+    cout << "namanya adalah " << venus.nama << endl;
+    cout << "diameter adalah " << venus.diameter << endl;
+    venus.displaydata();
 }
---
+++ coba2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class persegipanjang
+{
+public:
+    float panjang;
+    float lebar;
+    float luas;
+    void inputData()
+    {
+        cout << "input panjang : ";
+        cin >> panjang;
+        cout << "input lebar : ";
+        cin >> lebar;
+        
+    }
+    void menghitungluas(){
+        luas = panjang*lebar;
+        
+    }
+
+    void output()
+    {
+        cout << "luas persegi panjang = " << luas << endl;
+    }
+};
+
+int main()
+{
+    persegipanjang per;
+    per.inputData();
+    per.menghitungluas();
+    per.output();
+    return 0;
+}
